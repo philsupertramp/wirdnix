d32c4c9e9f924869ecae924334d30566a29291e1
Philipp Zettl <philipp.zettl@moberries.com>
2019-01-26 17:40:33 +0100

    add root

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "olcPixelGameEngine.h"
+// Override base class with your custom functionality
+class Example : public olc::PixelGameEngine
+{
+public:
+    Example()
+    {
+        sAppName = "Example";
+    }
+public:
+    bool OnUserCreate() override
+    {
+        // Called once at the start, so create things here
+        SetPixelMode(olc::Pixel::Mode::ALPHA);
+        return true;
+    }
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        // called once per frame, draws random coloured pixels
+
+        FillRect(rand() % 256, rand() % 240, 100, 100, olc::Pixel(rand() % 256, rand() % 256, rand() % 256, 10));
+        return true;
+    }
+};
+int main()
+{
+    Example demo;
+    if (demo.Construct(256, 240, 4, 4))
+        demo.Start();
+    return 0;
+}
