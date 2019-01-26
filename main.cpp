4de9ad1097baa8ae2505b663015bd8e6733fe638
Philipp Zettl <philipp.zettl@moberries.com>
2019-01-26 20:56:34 +0100

    add some functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "engine/olcPixelGameEngine.h"
+#include "Vec2D.h"
+#include "RenderObject.h"
 // Override base class with your custom functionality
 class Example : public olc::PixelGameEngine
 {
@@ -24,6 +26,10 @@ public:
 };
 int main()
 {
+    Vec2D a = Vec2D(2., 1.);
+    int32_t w = 10, h = 10;
+    RenderObject q = RenderObject(a, olc::Sprite(w, h));
+    std::cout << q.position.getX() << " " << q.position.getY() << std::endl;
     Example demo;
     if (demo.Construct(256, 240, 4, 4))
         demo.Start();
