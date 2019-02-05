882690a0685182b1f0e52a31e95cac1afe6d6a80
joshinils <joshua.laskowski@web.de>
2019-02-05 16:00:47 +0100

    make compile

--- util/Shell.cpp
+++ util/Shell.cpp
@@ -1,4 +1,96 @@
 #include "Shell.h"
+#include "Engine.h"
 
 const olc::Pixel Shell::Message::BACKGROUND_COLOR = olc::Pixel(0, 0, 0, 100);
-const float Shell::Message::MAX_AGE = 60;
+const float Shell::Message::MAX_AGE = 10;
+const float Shell::Message::FADE_AGE = MAX_AGE*2/3;
+
+Shell::Message::Message(std::string const & message, olc::Pixel color, uint32_t scale)
+    : _scale(scale)
+    , _originalMessage(message)
+    , _message(generateMessageString(_originalMessage))
+    , _messageSprite(uint32_t(Engine::ScreenWidth() * 2 / 3 + 2 - (Engine::ScreenWidth() * 2 / 3 + 1) % (8 * _scale)) + PADDING, uint32_t(_lines * 8 * _scale + 2))
+    , _color(color)
+    , _age(0)
+    , _backgroundColor(BACKGROUND_COLOR)
+{
+    initSprite();
+}
+
+void Shell::Message::initSprite()
+{
+    // reset to background color
+    for (uint32_t i = 0; i < _messageSprite.width; i++)
+    {
+        for (uint32_t j = 0; j < _messageSprite.height; j++)
+        {
+            _messageSprite.SetPixel(i, j, _backgroundColor);
+        }
+    }
+    Engine::DrawString(_messageSprite, 1, 1, _message, _color, _scale);
+}
+
+std::string Shell::Message::generateMessageString(std::string const & message)
+{
+    // 12 = 1.5(2/3 screen with) * 8(default font with)
+    messageWidth = uint32_t(Engine::ScreenWidth() / (12 * _scale));
+    _lines = 0;
+    std::string newString;
+
+    for (size_t start = 0; start < message.length(); start++)
+    {
+        std::string substr = message.substr(start, messageWidth);
+        size_t current_newlines = size_t(std::count(substr.begin(), substr.end(), '\n'));
+        size_t split_index = 0;
+        if (current_newlines == 0)
+        {
+            split_index = substr.find_last_of(' ');
+            if (split_index == std::string::npos)
+            {
+                split_index = messageWidth;
+            }
+            substr = substr.substr(0, split_index);
+            substr.append("\n");
+
+        }
+        newString.append(substr);
+        _lines += 1;
+        start += split_index;
+    }
+    if (_lines == 0) {
+        _lines = 1;
+    }
+    return newString;
+}
+
+void Shell::Message::draw(uint32_t height)
+{
+    Engine::instance().drawSpriteOnTop(&_messageSprite, Message::PADDING, height);
+}
+
+void Shell::draw(float fElapsedTime)
+{
+    uint32_t height = Engine::ScreenHeight() - Message::PADDING -messages.front().getMessageHeight();
+    for (auto& message : messages)
+    {
+        message.increaseAge(fElapsedTime);
+        message.draw(height);
+        height -= message.getMessageHeight() + Message::PADDING;
+
+        if (message.getAge() >= Message::FADE_AGE)
+        {
+            olc::Pixel c = message.getColor();
+            c.a -= /*(uint8_t)ceil((std::min(fElapsedTime, .99f)*c.a))+*/1;
+            message.setColor(c);
+
+            olc::Pixel b = message.getBackground();
+            b.a = c.a / 10;
+            message.setBackground(b);
+        }
+    }
+
+    messages.remove_if([](Message const& message)
+    {
+        return /*message.getAge() >= Message::MAX_AGE ||*/ message.getColor().a < 10;
+    });
+}
--- util/Shell.h
+++ util/Shell.h
@@ -5,12 +5,13 @@
 #include "Engine.h"
 #include <string>
 #include <list>
-
+#include <algorithm>
 
 class Shell
 {
 public:
     class Message;
+
 private:
     std::list<Message> messages;
     Shell() = default;
@@ -34,25 +35,24 @@ public:
         float _age;
 
     public:
-        static const olc::Pixel BACKGROUND_COLOR;
+        olc::Pixel _backgroundColor;
+        static const olc::Pixel Shell::Message::BACKGROUND_COLOR;
         static const uint32_t PADDING = 5;
         static const float MAX_AGE;
+        static const float FADE_AGE;
 
-        Message(std::string const& message, olc::Pixel color = olc::WHITE, uint32_t scale = 1)
-            : _scale(scale)
-            , _originalMessage(message)
-            , _message(generateMessageString(_originalMessage))
-            , _messageSprite(uint32_t(Engine::ScreenWidth() * 2 / 3 + 2 - (Engine::ScreenWidth() * 2 / 3 + 1)%(8*_scale)) + PADDING, uint32_t(_lines * 8 * _scale + 2) )
-            , _color(color)
-            , _age(0)
-        {
-            initSprite();
-        }
+        Message(std::string const& message, olc::Pixel color = olc::WHITE, uint32_t scale = 1);
 
         void setColor(olc::Pixel const& newColor)
         {
             _color = newColor;
             initSprite();
+        }
+
+        void setBackground(olc::Pixel const& newColor)
+        {
+            _backgroundColor = newColor;
+            initSprite(); //TODO dirty flag
         }
 
         void setScale(uint32_t newScale)
@@ -67,82 +67,39 @@ public:
             initSprite();
         }
 
-        void initSprite()
-        {
-            // reset to background color
-            for (uint32_t i = 0; i < _messageSprite.width; i++) {
-                for (uint32_t j = 0; j < _messageSprite.height; j++) {
-                    _messageSprite.SetPixel(i, j, BACKGROUND_COLOR);
-                }
-            }
-            Engine::DrawString(_messageSprite, 1, 1, _message, _color, _scale);
-        }
+        void initSprite();
 
         uint32_t getMessageHeight()
         {
             return _messageSprite.height;
         }
 
-        std::string generateMessageString(std::string const& message)
-        {
-            // 12 = 1.5(2/3 screen with) * 8(default font with)
-            messageWidth = uint32_t(Engine::ScreenWidth()/ (12 * _scale) );
-            _lines = 0;
-            std::string newString;
+        std::string generateMessageString(std::string const& message);
 
-            for(size_t start = 0; start < message.length(); start++)
-            {
-                std::string substr = message.substr(start, messageWidth);
-                size_t current_newlines = size_t(std::count(substr.begin(), substr.end(), '\n'));
-                size_t split_index = 0;
-                if(current_newlines == 0)
-                {
-                    split_index = substr.find_last_of(' ');
-                    if(split_index == std::string::npos)
-                    {
-                        split_index = messageWidth;
-                    }
-                    substr = substr.substr(0, split_index);
-                    substr.append("\n");
-
-                }
-                newString.append(substr);
-                _lines += 1;
-                start += split_index;
-            }
-            if(_lines == 0){
-                _lines = 1;
-            }
-            return newString;
-        }
-
-        void draw(uint32_t height)
-        {
-            Engine::instance().drawSpriteOnTop(&_messageSprite, height, Message::PADDING);
-        }
+        void draw(uint32_t height);
 
         void increaseAge(float fElapsedTime)
         {
             _age += fElapsedTime;
         }
 
-        float getAge()const {
+        float getAge() const
+        {
             return _age;
+        }
+
+        olc::Pixel const& getColor() const
+        {
+            return _color;
+        }
+
+        olc::Pixel const& getBackground() const
+        {
+            return _backgroundColor;
         }
     };
 
-    void draw(float fElapsedTime)
-    {
-        uint32_t height = Message::PADDING;
-        for(auto& message: messages){
-            message.increaseAge(fElapsedTime);
-            message.draw(height);
-            height += message.getMessageHeight() + Message::PADDING;
-        }
-        messages.remove_if([](Message const& message){
-            return message.getAge() >= Message::MAX_AGE;
-        });
-    }
+    void draw(float fElapsedTime);
 
     void addMessage(std::string const& message)
     {
@@ -152,7 +109,7 @@ public:
 
     void addMessage(Message const& message)
     {
-        messages.push_back(message);
+        messages.push_front(message);
     }
 };
 
